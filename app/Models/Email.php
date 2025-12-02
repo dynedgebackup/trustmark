f612ed315c2d4cc2349f0c85eddb51c3bba6395d
safik <safikalam123@gmail.com>
2025-12-02 11:39:26 +0530

    Statment cert.

--- app/Models/Email.php
+++ app/Models/Email.php
@@ -76,8 +76,8 @@ class Email extends Model
                     $pdf->SetAlpha(0.20);
                     $pdf->Image(public_path('assets/img/trustmark_logo.PNG'), 26, 46, 160, 200);
                     $pdf->SetAlpha(1);
-                    $barangays = DB::table('barangays')->select('id','brgy_description')->where('id',$business->barangay_id)->first();
-                    $complete_address = $barangays->brgy_description.', '.$business->complete_address;
+                    $barangays = DB::table('barangays')->select('id','reg_region','prov_desc','mun_desc','brgy_name')->where('id',$business->barangay_id)->first();
+                    $complete_address = $barangays->reg_region.', '.$barangays->prov_desc.', '.$barangays->mun_desc.', '.$barangays->brgy_name.', '.$business->complete_address;
                     $logoPath = public_path('assets/img/DTI-BP-transparent-statement.png');
                     $logo = 'data:image/png;base64,' . base64_encode(file_get_contents($logoPath));
                     // html load
