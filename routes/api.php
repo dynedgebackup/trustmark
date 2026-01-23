8105a5e4af74bb576ba3eeb28be2e6187081a940
Tushal <tushalburungale11@gmail.com>
2026-01-23 20:29:34 +0530

    webhook api changed

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SsoController;
 use App\Http\Controllers\Api\EmailTokenController;
-use App\Http\Controllers\WebhookController;
+use App\Http\Controllers\Api\WebhookApiController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -22,7 +22,7 @@ Route::prefix('email-tokens')->group(function () {
     Route::post('/time-until-next', [EmailTokenController::class, 'getTimeUntilNext']);
 });
 
-Route::any('/payment/webhook', [WebhookController::class, 'handle'])->withoutMiddleware(['throttle:tlpe-webhook']);
+Route::any('/payment/webhook', [WebhookApiController::class, 'handle'])->withoutMiddleware(['throttle:tlpe-webhook']);
 
 // php artisan queue:work --queue=default --memory=128
 
